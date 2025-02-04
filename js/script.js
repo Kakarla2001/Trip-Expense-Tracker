ed32546a2c5436cb6a958adf3dae71b0b8c4ab1d
Kakarla2001 <kakarlasai102@gmail.com>
2025-02-05 03:02:30 +0530

    added page loads(DOMContentLoaded event)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const params = new URLSearchParams(window.location.search);
+    const tripName = params.get("trip");
+
+    if (!tripName) {
+        alert("Trip not found!");
+        window.location.href = "index.html";
+    }
+
+    document.getElementById("tripTitle").textContent = tripName;
+
+    loadPersons(tripName);
+    loadExpenses(tripName);
+});
